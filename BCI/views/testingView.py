e52db77b330b7f3b6df806bb7404e47962e78cc0
Ludwig von Feilitzen <ludwig.von.feilitzen@gmail.com>
2019-11-23 16:17:43 +0100

    Testing mode openable
    App dont crash when opening test mode. It is still empty.

--- BCI/views/testingView.py
+++ BCI/views/testingView.py
@@ -1,6 +1,20 @@
+from PyQt5.QtWidgets import (QApplication, QAction, QLineEdit,
+                            QLabel, QWidget, QMainWindow, QListWidget,
+                           QGridLayout,QPushButton, QGroupBox,
+                           QVBoxLayout, QToolButton, QHBoxLayout)
 
+class TestingWindow(QWidget):
+    def __init__(self, parent = None):
+        super().__init__()
+        self.parent = parent
+        self.initTesting()
+        self.show()
 
-class TestingWindow():
+    def initTesting():
+        testGrid = QGridLayout()
+        
+        self.setLayout(testGrid)
+
     def test():
         self.title = 'BCI Hamburg Software v1.0'
         self.setWindowTitle(self.title)
